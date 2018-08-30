48317f513b7054700e33fe7893cbc1e4783d75cf
gmw-zjw <gaomingwei2017@163.com>
2018-08-30 20:37:07 +0800

    修改路由配置

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,28 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card, Row, Col } from 'antd'
 import './index.less'
 
-export default class Home extends React.Component {
+export default class Home extends Component {
 
     render() {
-        // layout
-        const homeItemLayout = {
-            labelCol: {
-                xs: 24,
-                sm: 4
-            },
-            wrapperCol: {
-                xs: 24,
-                sm: 12
-            }
-        }
 
         return (
             <div className="home-wrap">
-                <Row>
-                    <Col span='12'>1</Col>
-                    <Col span='12'>2</Col>
-                </Row>
+                this is home
             </div>
         );
     }
